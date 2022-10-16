817c269bd21ca117a52b5a75b8928348f23c302e
Sourabh <sourabhpatole@outlook.com>
2022-10-16 15:25:03 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  Navbar,
+  Feed,
+  SearchFeed,
+  ChannelDetail,
+  VideoDetail,
+} from "./components";
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Box sx={{ backgroundColor: "#000" }}>
+          <Navbar />
+          <Routes>
+            <Route path="/" exact element={<Feed />} />
+            <Route path="/video/:id" element={<VideoDetail />} />
+            <Route path="/channel/:id" element={<ChannelDetail />} />
+            <Route path="/search/:searchTerm" element={<SearchFeed />} />
+          </Routes>
+        </Box>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import { Videos } from "./";
+import { useParams } from "react-router-dom";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="600" mb={2} sx={{ color: "white" }}>
+        Search Results for:
+        <span style={{ color: "#2eade0" }}>{searchTerm}</span> Videos
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+
+export default SearchFeed;
